f71ce53cf6481b85b723cbd555a0e9208146a9c7
Shailaja Kshirsagar <shailajakshirsagarr@gmail.com>
2025-07-24 00:15:14 +0530

    CRUD commit

--- src/main/java/com/mapping/App.java
+++ src/main/java/com/mapping/App.java
@@ -15,7 +15,7 @@ public class App
 {
     public static void main( String[] args )
     {
-    	
+    	//Add data 
         SessionFactory factory = HibernateUtility.getSessionFactory();
         Session session = factory.openSession();
         Transaction transaction = session.beginTransaction();
